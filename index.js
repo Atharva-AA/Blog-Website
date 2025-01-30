5b526c2a7a41d3de8fc3bb687eea0c1ad1eb3bd3
unknown <atharva.achirnekar@yhs.edu.in>
2025-01-30 21:18:35 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,88 @@
+import express from "express";
+import bodyParser from "body-parser";
+import fs from "fs";
+import { title } from "process";
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const classValue = "disabled";
+app.get("/", (req, res) => {
+    res.render('homepage.ejs',{tempclass:classValue, title:title});
+  });
+app.post("/submit",(res,req)=>{
+  const title = res.body.Title;
+  const content = res.body.content;
+  fs.writeFile('public/docs/title.txt', title, 'utf8', (err) => {
+    if (err) {
+      console.error('Error writing to file:', err);
+      return;
+    }
+    console.log('File has been saved successfully!');
+  });
+  fs.writeFile('public/docs/content.txt', content, 'utf8', (err) => {
+    if (err) {
+      console.error('Error writing to file:', err);
+      return;
+    }
+    console.log('File has been saved successfully!');
+  });
+  req.render('homepage.ejs',{tempclass:" ",
+    title:title,
+  });
+})
+app.get("/writeBlog",(res,req)=>{
+  req.render('createPost.ejs');
+})
+
+app.get("/blog1",(res,req)=>{
+  req.render('blog1.ejs');
+})
+app.get("/blog2",(res,req)=>{
+  req.render('blog2.ejs');
+})
+app.get("/blog3",(res,req)=>{
+  req.render('blog3.ejs');
+})
+
+app.get("/blog4", (req, res) => {
+  fs.readFile('public/docs/title.txt', 'utf8', (err, title) => {
+    if (err) {
+      console.error("Error reading title file:", err);
+      return res.status(500).send("Error reading title file");
+    }
+
+    fs.readFile('public/docs/content.txt', 'utf8', (err, content) => {
+      if (err) {
+        console.error("Error reading content file:", err);
+        return res.status(500).send("Error reading content file");
+      }
+
+      console.log(title);
+      console.log(content);
+
+      res.render('newblog.ejs', { blogtitle: title, blogtext: content });
+    });
+  });
+});
+app.post("/edit",(res,req)=>{
+  const content = res.body.blogarea;
+  const title = res.body.Title;
+  console.log(title);
+  fs.writeFile('public/docs/content.txt', content, 'utf8', (err) => {
+    if (err) {
+      console.error('Error writing to file:', err);
+      return;
+    }
+    console.log('File has been saved successfully!');
+  });
+  req.render('homepage.ejs',{tempclass:" ",
+    title:title,
+  });
+})
+  
+app.listen(port,()=>{
+    console.log(`Port is listening at Server ${port}`);
+})
